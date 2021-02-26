f074b0f10ad076a3ea6c7771950755ab25399e71
Jun_Lei <v-jlei@hotels.com>
2021-02-26 22:48:22 +0800

    complete all testcases

--- testcases/test_wework.py
+++ testcases/test_wework.py
@@ -8,6 +8,7 @@
 """
 import json
 from time import sleep
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -20,43 +21,50 @@ class TestWeWork:
         # self.driver = webdriver.Chrome(options=chrome_arg)
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(8)
-        self.vars = {}
 
     def teardown(self):
         self.driver.quit()
 
-    def test_wework(self):
+    @pytest.mark.skip
+    def test_register(self):
         """
         Sign in from homepage of WechatWork
         """
-        self.driver.get("https://work.weixin.qq.com/")
-        self.driver.find_element(By.XPATH, "//*[@class='index_top_operation_loginBtn']").click()
-        self.driver.find_element(By.XPATH, "//*[@class='login_registerBar_link']").click()
-        self.driver.find_element(By.XPATH, "//*[@id='corp_name']").send_keys("test")
-        sleep(6)
-        self.driver.close()
+        self.driver.get("https://work.weixin.qq.com/")  # original website
+        self.driver.find_element(By.XPATH, "//*[@class='index_top_operation_loginBtn']").click()  # click on login
+        self.driver.find_element(By.XPATH, "//*[@class='login_registerBar_link']").click()  # Company registration
+        self.driver.find_element(By.XPATH, "//*[@id='corp_name']").send_keys("test")  # company name
+        sleep(2)
 
-    def test_login_tmp(self):
+    @pytest.mark.skip
+    def test_login_with_debugger(self):
         """
         Reusing chrome browser by port
         """
-        self.driver.get("https://work.weixin.qq.com/wework_admin/frame")
-        self.driver.find_element(By.XPATH, "//*[@id='menu_contacts']").click()
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame")  # website with login user
+        self.driver.find_element(By.XPATH, "//*[@id='menu_contacts']").click()  # change to contact tab
+        self.driver.find_element(By.XPATH, "//*[@id='menu_apps']").click()  # change to app management tab
+        self.driver.find_element(By.XPATH, "//*[@id='menu_customer']").click()
+        self.driver.find_element(By.XPATH, "//*[@id='menu_manageTools']").click()
+        self.driver.find_element(By.XPATH, "//*[@id='menu_profile']").click()
 
-    def test_cookie_login(self):
+    # @pytest.mark.skip
+    def test_login_with_cookies(self):
         """
         Access to website with cookies
         """
-        # store cookie
+        # store cookie, login status first with debugger
         # cookies = self.driver.get_cookies()
-        # with open("tmp2.text","w", encoding="utf-8") as f:
+        # print(cookies)
+        # with open("temp.txt", "w", encoding="utf-8") as f:
         #     json.dump(cookies, f)
 
-        # read cookie
+        # read cookie, could use general chromedriver
         self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
-        with open("tmp2.text", "r", encoding="utf-8") as f:
+        with open("temp.txt", "r", encoding="utf-8") as f:
             cookies = json.load(f)
-        for i in cookies:
-            self.driver.add_cookie(i)
+        print(cookies)
+        for cookie in cookies:
+            self.driver.add_cookie(cookie)
         self.driver.refresh()
-        sleep(6)
+        sleep(2)
